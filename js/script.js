e4e662733df70296ad7792cdf562ee393e9984c2
Foysal Hasib <118272136+foysalhasib@users.noreply.github.com>
2024-02-17 21:50:02 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,24 @@
+//=========================== Functions_Parameter/argument ========================================
+                    
+// function saySomething(hasib){
+//      console.log(`Hello Sumaiya ${hasib}`)
+// }
+
+// saySomething("Do you know me?");
+
+
+// ======================================================================
+// function saySomething(FirstName="Foysal",LastName="Hasib"){
+//      console.log(`Hello             ${FirstName} ${LastName}`)
+// }
+
+// saySomething();
+
+
+//=========================== Functions_Expression==========================
+
+let number = function() {
+          console.log(2024)
+}
+
+number();
